7aca8e8e95de57b1d212364fc3fe4e2e85cc6682
c4ei <c4ei.net@gmail.com>
2022-12-23 10:48:33 +0900

    wow another src

--- client/src/socket.js
+++
@@ -1,8 +0,0 @@
-import io from 'socket.io-client';
-// const sockets = io('https://localhost:3001', { autoConnect: true, forceNew: true });
-// const sockets = io('/');
-const sockets = io("https://chat.c4ei.net", {
-  path: "/chat/"
-});
-
-export default sockets;
---
+++ config/config.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE_TEST,
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+  },
+};
+// {
+//   "development": {
+//     "username": "root",
+//     "password": "root",
+//     "database": "chat_app",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   },
+//   "test": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_test",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   },
+//   "production": {
+//     "use_env_variable": "JAWSDB_URL",
+//     "dialect": "mysql"
+//   }
+// }
