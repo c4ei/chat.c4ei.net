2cad543d3361b08bbce2b8c5ef05d32d1a627378
c4ei <c4ei.net@gmail.com>
2022-12-26 11:09:38 +0900

    ing

--- config/config.js
+++ config/config.js
@@ -23,23 +23,3 @@ module.exports = {
     dialect: 'mysql',
   },
 };
-// {
-//   "development": {
-//     "username": "root",
-//     "password": "root",
-//     "database": "chat_app",
-//     "host": "127.0.0.1",
-//     "dialect": "mysql"
-//   },
-//   "test": {
-//     "username": "root",
-//     "password": null,
-//     "database": "database_test",
-//     "host": "127.0.0.1",
-//     "dialect": "mysql"
-//   },
-//   "production": {
-//     "use_env_variable": "JAWSDB_URL",
-//     "dialect": "mysql"
-//   }
-// }
